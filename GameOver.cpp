0c7fd0b538f336a80a7d651beeae899b577d985e
matheosantaq-dev <matheo.santaq@udea.edu.co>
2026-03-19 14:39:46 -0500

    Update GameOver.cpp

--- GameOver.cpp
+++ GameOver.cpp
@@ -1,1 +1,15 @@
+#include "gameover.h"
+bool GameOver(TETRIS &t, PIEZA &p){
 
+    // Verifica si la pieza nueva colisiona al aparecer
+    for(int i=0;i<4;i++){
+
+        int x = p.posX + p.bloques[i].x;
+        int y = p.posY + p.bloques[i].y;
+
+        if(leerBit(t,y,x))
+            return true;
+    }
+
+    return false;
+}
