d7e1b5ab11e13726e6db73d9465e600dfc53c2cb
JuanjoCardona12 <jose.cardona10@udea.edu.co>
2026-03-18 19:27:19 -0500

    main.cpp version final

--- main.cpp
+++ main.cpp
@@ -1,18 +1,36 @@
 #include "tetris.h"
+#include "pieza.h"
+#include <iostream>
 
-int main(){
+using namespace std;
 
+int main() {
     TETRIS t;
+    PIEZA p;
 
     crearTetris(t);
-
     inicializarTetris(t);
-
     crearBordes(t);
+    nuevaPiezaAleatoria(t, p);
 
-    dibujarTetris(t);
+    char tecla;
+    bool finDelJuego = false;
 
-    liberarTetris(t);
+    while (!finDelJuego) {
+        dibujarTetris(t);
+        cout << "Accion: [A]Izq [D]Der [S]Bajar [W]Rotar [Q]Salir: ";
+        cin >> tecla;
 
+        switch (tecla) {
+            case 'a': moverIzquierda(t, p); break;
+            case 'd': moverDerecha(t, p); break;
+            case 's': finDelJuego = moverAbajo(t, p); break;
+            case 'w': rotarDerecha(t, p); break;
+            case 'q': finDelJuego = true; break;
+        }
+    }
+
+    cout << "--- GAME OVER ---" << endl;
+    liberarTetris(t); // Limpieza de memoria dinámica
     return 0;
 }
